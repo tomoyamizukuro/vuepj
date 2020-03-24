d65b15d1b2224a724f6065fad3168b348dae0f36
tomoyamizukuro <salvatoleyohane@gmail.com>
2020-03-25 01:10:04 +0900

    Update リファクタリング、ゲーム終了時に勝敗のチェック機能追加

--- Setup/app.js
+++ Setup/app.js
@@ -11,6 +11,47 @@ var app = new Vue({
         // ゲーム開始時にヘルスゲージを初期化する
         this.playerHealth = 100;
         this.monsterHealth = 100;
+      },
+      attack: function (){
+        let damage = this.calculateDamage(3, 10);
+        this.monsterHealth -= this.calculateDamage(3, 10);
+        if(this.checkWin()){
+          return;
+        }
+
+        this.playerHealth -= this.calculateDamage(2, 12);
+        if(this.checkWin()){
+          return;
+        }
+      },
+      specialAttack: function(){
+
+      },
+      heal: function(){
+
+      },
+      giveUp: function(){
+
+      },
+      calculateDamage: function(min, max){
+      return  Math.max(Math.floor(Math.random() * max) + 1, min);
+      },
+      checkWin(){
+        if(this.monsterHealth <= 0){
+          if(confirm('you won! new game?')){
+            this.startGame();
+          } else {
+            this.gameIsRunning = false;
+          }
+          return true;
+          } else if(this.playerHealth <= 0){
+          if(confirm('you lose! new game?')){
+            this.startGame();
+          } else {
+            this.gameIsRunning = false;
+          }
+          return true;
+        }
       }
     }
 })
