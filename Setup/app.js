084ba383a80ccc3011df672843ca9bb02f47b315
tomoyamizukuro <salvatoleyohane@gmail.com>
2020-03-24 23:48:07 +0900

    Update ゲーム開始時にゲージを初期化する処理実装

--- Setup/app.js
+++ Setup/app.js
@@ -4,5 +4,13 @@ var app = new Vue({
       playerHealth: 100,
       monsterHealth: 100,
       gameIsRunning: false
+    },
+    methods: {
+      startGame: function() {
+        this.gameIsRunning = true;
+        // ゲーム開始時にヘルスゲージを初期化する
+        this.playerHealth = 100;
+        this.monsterHealth = 100;
+      }
     }
 })
