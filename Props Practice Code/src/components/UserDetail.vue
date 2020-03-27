79fb4e6634fca389c650926582f695362c051c54
tomoyamizukuro <salvatoleyohane@gmail.com>
2020-03-27 22:32:11 +0900

    add new repositry

---
+++ Props Practice Code/src/components/UserDetail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>{{switchName()}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['myName'],
+    methods: {
+        switchName() {
+            return this.myName.split("").reverse().join("");
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
---
+++ vue-cli/src/components/Server/Servers.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+<script>
+export default {
+    data: function() {
+        return {
+          status: 'Critical'
+        }
+    },
+    methods: {
+      changeStatus: function() {
+        this.status = 'Normal'
+      }
+    }
+}
+</script>
